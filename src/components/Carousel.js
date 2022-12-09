3a670f8d594a3efb7e22231e756483a6efbee3e0
blasetortoise284 <116454993+blasetortoise284@users.noreply.github.com>
2022-12-09 00:12:42 -0500

    Add files via upload

---
+++ src/components/Carousel.js
@@ -0,0 +1,50 @@
+import { Carousel } from 'react-bootstrap';
+
+function UncontrolledExample() {
+    return (
+        <Carousel>
+            <Carousel.Item className="carouselCardBg">
+                <img
+                    className="d-block w-100"
+                    src={process.env.PUBLIC_URL + '../images/sketch_1.png'}
+                />
+                <Carousel.Caption className="carouselCardText">
+                    <h3>Team Member 1</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item className="carouselCardBg">
+                <img
+                    className="d-block w-100"
+                    src={process.env.PUBLIC_URL + '../images/sketch_2.png'}
+                />
+                <Carousel.Caption className="carouselCardText">
+                    <h3>Team Member 2</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item className="carouselCardBg">
+                <img
+                    className="d-block w-100"
+                    src={process.env.PUBLIC_URL + '../images/sketch_3.png'}
+                />
+                <Carousel.Caption className="carouselCardText">
+                    <h3>Team Member 3</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item className="carouselCardBg">
+                <img
+                    className="d-block w-100"
+                    src={process.env.PUBLIC_URL + '../images/sketch_4.png'}
+                />
+                <Carousel.Caption className="carouselCardText">
+                    <h3>Team Member 4</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+        </Carousel>
+    );
+
+}
+export default UncontrolledExample;
